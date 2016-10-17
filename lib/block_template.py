a7e8b3a82bca6e1440780504fb10598d9f4131aa
Nicolas Boskovic <nicolas.boskovic@coinfabrik.com>
2016-10-17 19:15:09 -0300

    BTC dev mode

--- lib/block_template.py
+++ lib/block_template.py
@@ -77,7 +77,10 @@ class BlockTemplate(halfnode.CBlock):
         self.curtime = data['curtime']
         self.timedelta = self.curtime - int(self.timestamper.time())
         self.merkletree = mt
-        self.target = util.uint256_from_compact(self.nBits)
+        if settings.RSK_DEV_MODE:
+            self.target = int(settings.BTC_DEV_TARGET)
+        else:
+            self.target = util.uint256_from_compact(self.nBits)
         if 'rsk_flag' in data:
             if settings.RSK_DEV_MODE:
                 self.rsk_target = int(settings.RSK_DEV_TARGET)
