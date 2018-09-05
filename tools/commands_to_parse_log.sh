f2fbfde081adada8b4f26fc3069f6992a9608220
Ignacio <ignacio@rsk.co>
2018-09-05 15:27:34 -0300

    New script to parse # of messages

---
+++ tools/commands_to_parse_log.sh
@@ -0,0 +1,32 @@
+ #!/bin/bash
+
+echo 'path/name for the log file?'
+read FILE_PATH
+
+echo 'getblocktemplate:'
+grep "BTC_BLOCK_RECEIVED_START" $FILE_PATH  | wc -l
+
+echo 'BTC submits ALL:'
+grep "BTC_SUBMITBLOCK" $FILE_PATH | wc -l
+
+echo 'BTC submits ACCEPTED:'
+grep "# Block" $FILE_PATH | grep "ACCEPTED" | wc -l
+
+echo 'BTC submits REJECTED:'
+grep "# Block" $FILE_PATH | grep "REJECTED" | wc -l
+
+echo 'RSK submits ALL:'
+grep "RSK_SUBMITBLOCK" $FILE_PATH | wc -l
+
+echo 'RSK submits ACCEPTED:'
+grep "# Submit to RSK, Block" $FILE_PATH | grep "ACCEPTED" |  wc -l
+
+echo 'RSK submits REJECTED:'
+grep "# Submit to RSK, Block" $FILE_PATH | grep "REJECTED" |  wc -l
+
+echo 'Shares:'
+grep "SHARE_RECEIVED_START" $FILE_PATH | wc -l
+
+echo "Errors ------------------" 
+echo 'SUBMIT - Duplicate share:'
+grep "SUBMIT EXCEPTION: Duplicate share" $FILE_PATH | wc -l
